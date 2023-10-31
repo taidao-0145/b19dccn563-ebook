07662ae6e63ae8390dfbfb3aeceacabe5552e1c5
tai-dao-0145 <tai.dao@tda.company>
2023-10-31 21:37:22 +0700

    add model book user

--- backend/app/Models/Book.php
+++ backend/app/Models/Book.php
@@ -4,22 +4,54 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'isbn',
+        'c_code',
+        'hash_id',
         'name',
         'description',
         'thumbnail',
         'author_name',
         'page_number',
-        'relase_date',
+        'cover_page',
+        'public_date',
+        'is_limit_public',
         'inventory_number',
         'price',
         'sample_path',
         'book_type',
     ];
+
+    public function getRouteKeyName()
+    {
+        return 'hash_id';
+    }
+
+    public function bookResources(): HasMany
+    {
+        return $this->hasMany(BookResource::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, BookCategory::class, 'book_id', 'category_id');
+    }
+
+    public function bookOrders(): BelongsToMany
+    {
+        return $this->belongsToMany(BookOrder::class, BookOrderItem::class, 'book_id', 'book_order_id');
+    }
+
+    public function usersAddToCart(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, BookCart::class, 'book_id', 'user_id');
+    }
 }
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Mail\VerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +25,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'address',
+        'gender',
+        'date_of_birth',
+        'stripe_customer_id',
     ];
 
     /**
@@ -41,4 +50,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new VerifyEmail());
+    }
+
+    public function bookOrders(): HasMany
+    {
+        return $this->hasMany(BookOrder::class);
+    }
+
+    public function booksInCart(): BelongsToMany
+    {
+        return $this->belongsToMany(Book::class, BookCart::class, 'user_id', 'book_id');
+    }
+
+    public function bookCarts(): HasMany
+    {
+        return $this->hasMany(BookCart::class, 'book_id', 'id');
+    }
 }
